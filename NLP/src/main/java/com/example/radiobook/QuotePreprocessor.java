cc333cc36fb0c43c610346b51e594ddfd438a5ce
AnjanaSenanayake <anjanajayabandara@gmail.com>
2020-02-17 22:26:59 +0530

    new classify and create quote algorithms

--- NLP/src/main/java/com/example/radiobook/QuotePreprocessor.java
+++ NLP/src/main/java/com/example/radiobook/QuotePreprocessor.java
@@ -12,7 +12,7 @@ public class QuotePreprocessor {
 
     private CoreDocument document;
     private Annotation annotation;
-    private int lambda = 3; // lambda an estimation of phrase about 3 or more words
+    private int lambda = 3; // lambda an estimation of a phrase about 3 or more words
     private String defaultSpeaker = "narrator";
     private final String TOKENIZER_REGEX = " ";
     private ArrayList<Quote> quoteList = new ArrayList<>();
@@ -29,35 +29,42 @@ public class QuotePreprocessor {
     //
     public Quote[] classify() {
         String prevSentence = "", currentSentence = "", prevSpeaker = "", currentSpeaker;
-        int prevQuoteLastIdx = 0, prevSentenceIdx = 0, currentSentenceIdx, quoteIdx, length;
+        int prevQuoteLastIdx = 0, prevQuoteLastSentenceIdx = 0, prevSentenceIdx = 0, currentSentenceIdx = 0, quoteStartingIdx, quoteLength = 0;
         boolean isTag;
         List<CoreQuote> quotes = document.quotes();
         for (CoreQuote quote : quotes) {
             currentSentence = quote.sentences().get(0).text();
             currentSentenceIdx = quote.sentences().get(0).coreMap().get(CoreAnnotations.SentenceIndexAnnotation.class);
-            length = (prevSentence.isEmpty()) ? 0 : prevSentence.length();
-            quoteIdx = getQuoteStartingIdx(currentSentence, quote.text());
-            if (prevSentenceIdx < currentSentenceIdx) {
-                isTag = isQuoteTag(prevSentence, prevQuoteLastIdx, length);
-                currentSpeaker = isTag ? defaultSpeaker : prevSpeaker;
-                // TODO check here
-                createQuote(prevSentenceIdx, prevSentenceIdx, prevQuoteLastIdx, length, currentSpeaker, isTag);
-                prevQuoteLastIdx = 0;
-                length = prevSentence.length();
-                // TODO check here
-                createQuote(prevSentenceIdx + 1, currentSentenceIdx - 1, prevQuoteLastIdx, length, defaultSpeaker, false);
-                prevSentenceIdx = currentSentenceIdx;
+            quoteStartingIdx = getQuoteStartingIdx(currentSentence, quote.text());
+
+            currentSpeaker = quote.hasSpeaker ? quote.speaker().get() : (quote.hasCanonicalSpeaker?quote.canonicalSpeaker().get():defaultSpeaker);
+            quoteLength = quoteStartingIdx + quote.text().length();
+
+            if ((currentSentenceIdx - prevSentenceIdx) == 1 && quoteStartingIdx == 0) {
+                if (quote.sentences().size() > 1) {
+                    int noOfQuoteSentences = quote.sentences().size();
+                    quoteList.add(createQuote(currentSentenceIdx, currentSentenceIdx + noOfQuoteSentences - 1, quoteStartingIdx, quoteLength, currentSpeaker, false, true));
+                } else {
+                    quoteList.add(createQuote(currentSentenceIdx, currentSentenceIdx, quoteStartingIdx, quoteLength, currentSpeaker, false, true));
+                }
+            } else {
+                quoteList.add(createQuote(prevSentenceIdx, currentSentenceIdx, prevQuoteLastIdx, quoteStartingIdx, defaultSpeaker, true, false));
+                if (quote.sentences().size() > 1) {
+                    int noOfQuoteSentences = quote.sentences().size();
+                    quoteList.add(createQuote(currentSentenceIdx, currentSentenceIdx + noOfQuoteSentences - 1, quoteStartingIdx, quoteLength, currentSpeaker, false, true));
+                } else {
+                    quoteList.add(createQuote(currentSentenceIdx, currentSentenceIdx, quoteStartingIdx, quoteLength, currentSpeaker, false, true));
+                }
             }
-            isTag = isQuoteTag(currentSentence, prevQuoteLastIdx, quoteIdx);
-            currentSpeaker = quote.speaker().get();
-            currentSpeaker = isTag ? currentSpeaker : defaultSpeaker;
-            // TODO check here
-            quoteList.add(createQuote(currentSentenceIdx, currentSentenceIdx, prevQuoteLastIdx, quoteIdx, currentSpeaker, isTag));
-            prevQuoteLastIdx = quote.text().length() - 1;
-            // TODO check here
-            createQuote(currentSentenceIdx, currentSentenceIdx, quoteIdx, prevQuoteLastIdx, currentSpeaker, isTag);
-            prevSpeaker = currentSpeaker;
-            prevSentence = currentSentence;
+
+            prevQuoteLastSentenceIdx = currentSentenceIdx + quote.sentences().size() - 1;
+            prevSentenceIdx = currentSentenceIdx;
+            prevQuoteLastIdx = quoteStartingIdx + quote.text().length();
+            //prevSpeaker = currentSpeaker;
+        }
+        int lastDocumentSentenceIdx = document.sentences().size() - 1;
+        if (prevQuoteLastSentenceIdx < lastDocumentSentenceIdx) {
+            quoteList.add(createQuote(currentSentenceIdx, lastDocumentSentenceIdx, prevQuoteLastIdx, 0, defaultSpeaker, true, false));
         }
         return null;
     }
@@ -80,19 +87,36 @@ public class QuotePreprocessor {
     // quote
     // speaker: quote’s speaker
     // tag: is a quote attribution tag
-    private Quote createQuote(int startingSentenceIdx, int endingSentenceIdx, int startingQuoteIdx, int endingQuoteIdx, String speaker, boolean isTag) {
-        String startingSentence = getSentenceByIndex(startingSentenceIdx);
+    private Quote createQuote(int startingSentenceIdx, int endingSentenceIdx, int startingQuoteIdx, int endingQuoteIdx, String speaker, boolean isTag, boolean isQuote) {
+        //String startingSentence = getSentenceByIndex(startingSentenceIdx);
         String endingSentence = getSentenceByIndex(endingSentenceIdx);
         StringBuffer strBuffer = new StringBuffer();
-        if (startingSentenceIdx == endingSentenceIdx) {
-            strBuffer.append(startingSentence, startingQuoteIdx, endingQuoteIdx);
-        } else {
-            strBuffer.append(startingSentence.substring(startingQuoteIdx));
-            for (int i = startingSentenceIdx + 1; i < endingQuoteIdx; i++) {
-                strBuffer.append(document.sentences().get(i).text());
+
+        if (isQuote) {
+            if (startingSentenceIdx == endingSentenceIdx) {
+                strBuffer.append(endingSentence, startingQuoteIdx, endingQuoteIdx);
+            } else {
+                StringBuffer tempBuffer = new StringBuffer();
+                for (int i = startingSentenceIdx; i <= endingSentenceIdx; i++) {
+                    tempBuffer.append(document.sentences().get(i).text()).append(" ");
+                }
+                strBuffer.append(tempBuffer.substring(startingQuoteIdx));
             }
-            strBuffer.append(endingSentence, 0, endingQuoteIdx);
+        } else {
+            if (startingSentenceIdx == endingSentenceIdx) {
+                strBuffer.append(endingSentence, startingQuoteIdx, endingQuoteIdx);
+            } else {
+                StringBuffer tempBuffer = new StringBuffer();
+                for (int i = startingSentenceIdx; i < endingSentenceIdx; i++) {
+                    tempBuffer.append(document.sentences().get(i).text()).append(" ");
+                }
+                strBuffer.append(tempBuffer.substring(startingQuoteIdx));
+
+                if (endingQuoteIdx > 0)
+                    strBuffer.append(endingSentence, 0, endingQuoteIdx);
+            }
         }
+
         if (strBuffer.length() > 0)
             return new Quote(strBuffer.toString(), speaker, isTag);
         else
@@ -101,7 +125,7 @@ public class QuotePreprocessor {
 
     // Returns the starting index of a quote in a sentence
     private int getQuoteStartingIdx(String sentence, String quote) {
-        return sentence.indexOf(quote);
+        return Math.max(sentence.indexOf(quote), 0);
     }
 
     private String getSentenceByIndex(int index) {
@@ -111,5 +135,16 @@ public class QuotePreprocessor {
             return new CoreDocument(annotation).sentences().get(index).text();
         else
             return null;
+    }
+
+    public void printQuotes() {
+        for (Quote quote : quoteList) {
+            if (quote != null) {
+                System.out.println("Quote: " + quote.getQuote());
+                System.out.println("Speaker: " + quote.getSpeaker());
+                System.out.println("IsTag: " + quote.isQuoteAttributionTag());
+                System.out.println();
+            }
+        }
     }
 }
