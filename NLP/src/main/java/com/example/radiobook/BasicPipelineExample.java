95445b7592004aa780729ebef3f248e6e9b53520
AnjanaSenanayake <anjanajayabandara@gmail.com>
2020-02-22 16:46:30 +0530

    sepaker voice asignment implemented

--- NLP/src/main/java/com/example/radiobook/BasicPipelineExample.java
+++ NLP/src/main/java/com/example/radiobook/BasicPipelineExample.java
@@ -15,7 +15,8 @@ public class BasicPipelineExample {
     //public static String text = "Hal woke with a start. He found himself sitting up in bed, his spine tingling. What had roused him? A cry of some sort. The play of light and shadow in the tent told him that the camp-fire outside was still burning. It was meant to keep off dangerous visitors. Wild animals were all about -yet the sound he had heard did not seem the voice of an animal. Still, he could be mistaken. This was his first night in the African wilds. Beside the camp-fire earlier in the evening he and his younger brother, Roger, had listened to the voices of the forest while their Father, John Hunt, told them what they were hearing. Hunt had said, ‘It’s like an orchestra,’. ‘Those high violins you hear are being played by the jackals. That crazy trombone - the hyena is playing it. The hippo is on the bass tuba. Doesn’t that wart-hog’s 'arnk-arnk-arnk' sound just like a snare drum? And listen - far away … you can just hear it, a lion on the 'cello'.’ ‘Who’s that with the saxophone?’ asked Roger. ‘The elephant. He’s good on the trumpet too.’ A sharp grinding roar made the boys jump. Whatever made it was very close to the camp. It sounded like a rough file being dragged over the edge of a tin roof.";
     //public static String text = "Roger was awake. Hal woke with saying, ‘What will happen to us?’ Hal looked worried. Hunt said ‘Calm down all’.";
     //public static String text = "‘You always look so cool,’ Daisy repeated innocently. She had told him that she loved him, and Tom Buchanan saw. He exclaimed. His mouth opened a little and he looked at Gatsby and then back at Daisy as if he had just recognized her as someone he knew a long time ago. ‘You resemble the advertisement of the man,’ she went on innocently ‘You know the advertisement of the man’";
-    //public static String text = "Mary was crying. ‘I want ice cream’, she repeated aloud.";
+    //public static String text = "‘I want ice cream,’ Ann cried aloud. Mary angrily said ‘Do your home work, then you can have it’.";
+    //public static String text = "‘Ah’ said he, ‘I have this load to carry: to be sure it is silver, but it is so heavy that I can’t hold up my head, and you must know it hurts my shoulder sadly.’";
     public static String text;
     public static void main(String[] args) {
         
@@ -23,7 +24,7 @@ public class BasicPipelineExample {
         // reading from a file 
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader("story_books/HANS_IN_LUCK_THE_BROTHERS_GRIMM_FAIRY_TALES.txt"));
+            reader = new BufferedReader(new FileReader("story_books/THE_GOLDEN_BIRD_THE_BROTHERS_GRIMM_FAIRY_TALES.txt"));
             StringBuilder sb = new StringBuilder();
             String line = reader.readLine();
             while (line != null) {
@@ -74,6 +75,7 @@ public class BasicPipelineExample {
         QuotePreprocessor quotePreprocessor = new QuotePreprocessor(document);
         quotePreprocessor.generateEmotionsMap();
         quotePreprocessor.classify();
+        quotePreprocessor.polishIsTagSpeaker();
         quotePreprocessor.printQuotes();
         quotePreprocessor.printSpeakerGenders();
 
